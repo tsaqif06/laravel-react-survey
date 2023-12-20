0468124a20fe92e9b16c337996ed789ec4d7decd
Ahmad Tsaqif Al Arifin <tsaqifcakep87@gmail.com>
2023-12-20 19:56:44 +0700

    add auth controller axios

--- react/src/axios.js
+++ react/src/axios.js
@@ -4,4 +4,22 @@ const axiosClient = axios.create({
   baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`,
 });
 
+axiosClient.interceptors.request.use((config) => {
+  const token = "123"; //TODO
+  config.headers.Authorization = `Bearer ${token}`;
+});
+
+axiosClient.interceptors.response.use(
+  (resp) => {
+    return resp;
+  },
+  (error) => {
+    if (error.resp && error.resp.status === 401) {
+      router.navigate("/login");
+      return error;
+    }
+    throw error;
+  }
+);
+
 export default axiosClient;
