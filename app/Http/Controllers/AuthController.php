efe9fbf58b6ee5fb6f5936b9b56fde0ac7bbf2bf
Ahmad Tsaqif Al Arifin <tsaqifcakep87@gmail.com>
2023-12-27 10:42:54 +0700

    add logout method and import auth class

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\SignupRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -50,5 +51,13 @@ class AuthController extends Controller
     }
     public function logout(Request $request)
     {
+        /** @var User $user */
+        $user = Auth::user();
+        $user->currentAccessToken()->delete();
+
+
+        return response([
+            'success' => true,
+        ]);
     }
 }
