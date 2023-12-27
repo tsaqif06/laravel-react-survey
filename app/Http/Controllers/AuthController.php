1e9a3629580a91b86a98f0d0fa943483381ddcb5
Ahmad Tsaqif Al Arifin <tsaqifcakep87@gmail.com>
2023-12-27 10:32:17 +0700

    add signup method

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\SignupRequest;
 use Illuminate\Http\Request;
@@ -10,15 +11,27 @@ class AuthController extends Controller
 {
     public function signup(SignupRequest $request): void
     {
-        
+        $data = $request->validated();
+
+        /** @var \App\Models\User $user */
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+        ]);
+
+        $token = $user->createToken('main')->plainTextToken;
+
+        return response([
+            'user' => $user,
+            'token' => $token,
+        ]);
     }
 
     public function login(LoginRequest $request): void
     {
-
     }
     public function logout(Request $request): void
     {
-
     }
 }
