3517d52e34c2fd97f151af0fee79178b10c62bff
Ahmad Tsaqif Al Arifin <tsaqifcakep87@gmail.com>
2024-01-26 14:24:32 +0700

    add crud controller

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\SurveyController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +18,7 @@ use App\Http\Controllers\AuthController;
 
 Route::middleware('auth:sanctum')->group(function() {
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::apiResource('survey', SurveyController::class);
 });
 
 Route::post('/signup', [AuthController::class, 'signup']);
